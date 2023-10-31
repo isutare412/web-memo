9373b6dab5db69a109f0e4c7e64ad0eef5b2813b
Redshore <isutare412@gmail.com>
2023-10-31 21:31:05 +0900

    feat: edit memo page

--- ui/src/routes/[memoId=uuid]/+page.svelte
+++ ui/src/routes/[memoId=uuid]/+page.svelte
@@ -1,15 +1,38 @@
 <script lang="ts">
+  import { goto } from '$app/navigation'
   import { page } from '$app/stores'
   import Tag from '$components/Tag.svelte'
+  import { deleteMemo } from '$lib/apis/backend/memo'
   import { memoStore, syncMemo } from '$lib/memo'
+  import { addToast } from '$lib/toast'
+  import { getErrorMessage } from '$lib/utils/error'
   import { onMount } from 'svelte'
 
   $: memoId = $page.params.memoId
   $: memo = $memoStore.memos.find((memo) => memo.id === memoId)
 
   onMount(async () => {
-    await syncMemo(memoId)
+    try {
+      await syncMemo(memoId)
+    } catch (error) {
+      addToast(getErrorMessage(error), 'error')
+      goto('/')
+      return
+    }
   })
+
+  function onEditClick() {
+    if (memo === undefined) return
+
+    goto(`/${memoId}/edit`)
+  }
+
+  async function onDeleteClick() {
+    if (memo === undefined) return
+
+    await deleteMemo(memoId)
+    goto('/', { replaceState: true })
+  }
 </script>
 
 {#if memo !== undefined}
@@ -23,5 +46,9 @@
   {/if}
   <div class="mt-3">
     <span class="whitespace-pre-wrap">{memo.content}</span>
+  </div>
+  <div class="mt-4 flex justify-end gap-x-1">
+    <button on:click={onEditClick} class="btn btn-primary btn-sm">Edit</button>
+    <button on:click={onDeleteClick} class="btn btn-primary btn-sm">Delete</button>
   </div>
 {/if}
---
+++ ui/src/routes/[memoId=uuid]/edit/+page.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+  import { goto } from '$app/navigation'
+  import { page } from '$app/stores'
+  import MemoEditor from '$components/MemoEditor.svelte'
+  import { replaceMemo } from '$lib/apis/backend/memo'
+  import { memoStore, syncMemo } from '$lib/memo'
+  import { addToast } from '$lib/toast'
+  import { getErrorMessage } from '$lib/utils/error'
+  import { onMount } from 'svelte'
+
+  $: memoId = $page.params.memoId
+  $: memo = $memoStore.memos.find((memo) => memo.id === memoId)
+
+  $: title = memo?.title ?? ''
+  $: content = memo?.content ?? ''
+  $: tags = memo?.tags ?? []
+
+  onMount(async () => {
+    try {
+      await syncMemo(memoId)
+    } catch (error) {
+      addToast(getErrorMessage(error), 'error')
+      goto('/')
+      return
+    }
+  })
+
+  async function onMemoSubmit(
+    event: CustomEvent<{ title: string; content: string; tags: string[] }>
+  ) {
+    try {
+      await replaceMemo({
+        id: memoId,
+        title: event.detail.title,
+        content: event.detail.content,
+        tags: event.detail.tags,
+      })
+    } catch (error) {
+      addToast(getErrorMessage(error), 'error')
+      return
+    }
+
+    goto('/')
+  }
+</script>
+
+<MemoEditor {title} {content} {tags} on:submit={onMemoSubmit} />
