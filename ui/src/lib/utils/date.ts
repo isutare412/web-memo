d4e4b9c63053bad72f5df177e370394129034017
Redshore <isutare412@gmail.com>
2023-11-03 10:44:37 +0900

    fix: use correct month

--- ui/src/lib/utils/date.ts
+++ ui/src/lib/utils/date.ts
@@ -1,6 +1,6 @@
 export function formatDate(date: Date): string {
   const year = date.getFullYear()
-  const month = date.getMonth().toString().padStart(2, '0')
+  const month = (date.getMonth() + 1).toString().padStart(2, '0')
   const day = date.getDate().toString().padStart(2, '0')
   const hour = date.getHours().toString().padStart(2, '0')
   const minute = date.getMinutes().toString().padStart(2, '0')
