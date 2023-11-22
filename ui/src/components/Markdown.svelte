6a93ec53b1557c7c6e43f8cbb8605241438cdfcf
Redshore <isutare412@gmail.com>
2023-11-23 08:59:01 +0900

    feat: sanitize html before rendenring

--- ui/src/components/Markdown.svelte
+++ ui/src/components/Markdown.svelte
@@ -1,9 +1,14 @@
 <script lang="ts">
   import { Marked } from '@ts-stack/markdown'
+  import DOMPurify from 'dompurify'
 
   export let content: string
+
+  const forbiddenTags = ['form', 'button']
 </script>
 
 <article class="prose break-words">
-  {@html Marked.parse(content, { breaks: true })}
+  {@html DOMPurify.sanitize(Marked.parse(content, { breaks: true }), {
+    FORBID_TAGS: forbiddenTags,
+  })}
 </article>
