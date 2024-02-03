71c0916759d7ee263affc786a6c96585a48f2931
Redshore <isutare412@gmail.com>
2024-02-03 18:22:04 +0900

    chore: add open graph metadata

--- ui/src/routes/+layout.svelte
+++ ui/src/routes/+layout.svelte
@@ -1,13 +1,13 @@
 <script lang="ts">
+  import { navigating } from '$app/stores'
   import Avatar from '$components/Avatar.svelte'
+  import LoadingSpinner from '$components/LoadingSpinner.svelte'
   import ThemeToggle from '$components/ThemeToggle.svelte'
   import ToastContainer from '$components/ToastContainer.svelte'
   import { setDocumentDataTheme, setPreferredTheme, type ThemeMode } from '$lib/theme'
   import '@fontsource-variable/inter'
   import '../app.css'
   import type { LayoutData } from './$types'
-  import { navigating } from '$app/stores'
-  import LoadingSpinner from '$components/LoadingSpinner.svelte'
 
   export let data: LayoutData
 
@@ -19,6 +19,11 @@
 
 <svelte:head>
   <title>Web Memo</title>
+  <meta property="og:title" content="Web Memo" />
+  <meta
+    property="og:image"
+    content="https://raw.githubusercontent.com/isutare412/web-memo/2fcfcb1360609d3a5a9ed5aa460421338784ca4e/ui/static/android-chrome-512x512.png"
+  />
 </svelte:head>
 
 <div class="flex min-h-screen flex-col">
