b266eb74fd56f474ac1b281ccdf7df57d784364f
Redshore <isutare412@gmail.com>
2024-02-22 13:25:17 +0900

    feat: add bookmark button

---
+++ ui/src/components/SubscribeButton.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import BookmarkIcon from '$components/icons/BookmarkIcon.svelte'
+  import { createEventDispatcher } from 'svelte'
+
+  export let isActivated: boolean
+
+  const dispatch = createEventDispatcher()
+
+  function onClickSubscribeButton() {
+    dispatch('subscribe', { isSubscribe: !isActivated })
+  }
+</script>
+
+<div class:opacity-70={!isActivated}>
+  <button
+    on:click={onClickSubscribeButton}
+    class="btn btn-sm btn-outline rounded-full"
+    class:btn-primary={isActivated}
+  >
+    <div class="w-[12px]">
+      <BookmarkIcon />
+    </div>
+  </button>
+</div>
