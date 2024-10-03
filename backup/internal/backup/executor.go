ad3b1ef7a0fb3c2926b3ba54a4086594ff432243
Redshore <isutare412@gmail.com>
2024-10-03 20:44:35 +0900

    feat: include stderr message in error

--- backup/internal/backup/executor.go
+++ backup/internal/backup/executor.go
@@ -24,27 +24,31 @@ func NewExecutor() *Executor {
 func (e *Executor) BackupDatabase(ctx context.Context, req model.DatabaseBackupRequest) (backup io.Reader, err error) {
 	cmd := exec.CommandContext(ctx, e.pgDump)
 
-	stdout := new(bytes.Buffer)
-	setBackupVariables(cmd, req, stdout)
+	var (
+		stdout = new(bytes.Buffer)
+		stderr = new(bytes.Buffer)
+	)
+	setBackupVariables(cmd, req, stdout, stderr)
 
 	if err := cmd.Run(); err != nil {
-		return nil, fmt.Errorf("executing pg_dump: %w", err)
+		return nil, fmt.Errorf("executing pg_dump: %w: %s", err, stderr.String())
 	}
 
 	return stdout, nil
 }
 
-func setBackupVariables(cmd *exec.Cmd, req model.DatabaseBackupRequest, destination io.Writer) {
+func setBackupVariables(cmd *exec.Cmd, req model.DatabaseBackupRequest, stdout, stderr io.Writer) {
 	cmd.Env = []string{
 		fmt.Sprintf("PGPASSWORD=%s", req.Password),
 	}
 
-	cmd.Args = []string{
+	cmd.Args = append(cmd.Args,
 		"-h", req.Host,
 		"-p", strconv.Itoa(req.Port),
 		"-U", req.User,
 		req.DatabaseName,
-	}
+	)
 
-	cmd.Stdout = destination
+	cmd.Stdout = stdout
+	cmd.Stderr = stderr
 }
