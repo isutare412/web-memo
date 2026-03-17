a49ed663a30bee406dbdce805cae12a13183517c
Redshore <isutare412@gmail.com>
2026-03-17 21:46:54 +0900

    chore: bump up otel semconv version

--- api/internal/tracing/peer_service.go
+++ api/internal/tracing/peer_service.go
@@ -1,13 +1,13 @@
 package tracing
 
-import semconv "go.opentelemetry.io/otel/semconv/v1.37.0"
+import semconv "go.opentelemetry.io/otel/semconv/v1.40.0"
 
 var (
-	PeerServiceGoogleOIDC = semconv.PeerService("google-oidc")
-	PeerServicePostgres   = semconv.PeerService("postgres")
-	PeerServiceRedis      = semconv.PeerService("redis")
-	PeerServiceInternet   = semconv.PeerService("internet")
-	PeerServiceTEI        = semconv.PeerService("huggingface-tei-qwen3-embedding")
-	PeerServiceBM25       = semconv.PeerService("huggingface-bm25")
-	PeerServiceQdrant     = semconv.PeerService("qdrant")
+	PeerServiceGoogleOIDC = semconv.ServicePeerName("google-oidc")
+	PeerServicePostgres   = semconv.ServicePeerName("postgres")
+	PeerServiceRedis      = semconv.ServicePeerName("redis")
+	PeerServiceInternet   = semconv.ServicePeerName("internet")
+	PeerServiceTEI        = semconv.ServicePeerName("huggingface-tei-qwen3-embedding")
+	PeerServiceBM25       = semconv.ServicePeerName("huggingface-bm25")
+	PeerServiceQdrant     = semconv.ServicePeerName("qdrant")
 )
